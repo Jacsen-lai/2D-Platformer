b0814e1c76b0899e47305866c412e89975c45530
Jacsen-lai <rapidjacsen@gmail.com>
2025-04-27 13:18:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,23 @@ import sys
 
 import pygame
 
-pygame.init()
+class Game:
+    def __init__(self):
+        pygame.init()
 
-screen = pygame.display.set_mode((640,480))
+        self.screen = pygame.display.set_mode((640,480))
 
-clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        pygame.display.set_caption("NEA Jacsen Lai Teleporting Platformer")
 
-    pygame.display.update()
-    clock.tick(60)        
+    def run(self):
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                pygame.display.update()
+                self.clock.tick(60)        
