55c79891ddd49db937928f31d8befd4dd90ab42a
Jacsen-lai <rapidjacsen@gmail.com>
2025-09-17 11:48:31 +0100

    resize image

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -6,3 +6,4 @@ class Tilemap:
 
         for i in range(10):
             self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
+            self.tilemap[';10' + str(i + 5)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
