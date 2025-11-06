02f08e9c8ed541c100707f8560f8206513371635
Jacsen-lai <rapidjacsen@gmail.com>
2025-11-06 13:14:49 +0000

    reverted jump to infinite for now

--- scripts/entities.py
+++ scripts/entities.py
@@ -52,39 +52,12 @@ class PhysicsEntity:
                 # IMPORTANT: pos[1] is the rect.top (since pos = top-left)
                 self.pos[1] = entity_rect.top
 
-class Player(PhysicsEntity):
-    def __init__(self, game, pos, size):
-        super().__init__(game, 'player', pos, size)
-        self.air_time = 0
-        self.jumps = 1
-
-    def update(self, tilemap, movement=(0, 0)):
-        super().update(tilemap, movement=movement)
-
-        self.air_time += 1
-        if self.collisions['down']:
-            self.air_time = 0
-            self.jumps = 1
-            
-        if self.air_time > 4:
-            self.set_action('jump')
-        elif movement[0] != 0:
-            self.set_action('run')
-        else:
-            self.set_action('idle')
-
-    def jump(self):
-        if self.jumps:
-            self.velocity[1] = -3
-            self.jumps -= 1
-            self.air_time = 5
 
         #Gravity
         if not self.grounded:
             self.velocity[1] = min(5, self.velocity[1] + 0.1)
         else:
-            self.velocity[1] = 0  # keep grounded, no sinking
-
+            self.velocity[1] = 0  # 
     def render(self, surf, offset=(0, 0)):
         surf.blit(self.game.assets['player'], (self.pos[0] - offset[0], self.pos[1] - offset[1]))
 
