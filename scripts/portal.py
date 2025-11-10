d4a6893c401427728dac622ca1e78cef8e5e78c5
Jacsen-lai <rapidjacsen@gmail.com>
2025-11-10 21:12:13 +0000

    changed portal

--- scripts/portal.py
+++ scripts/portal.py
@@ -14,10 +14,8 @@ class Portal:
         self.outline_color = (15, 40, 20)   # dark moss outline
         self.rect = pygame.Rect(self.pos[0], self.pos[1], self.size[0], self.size[1])
 
-        self.animation_time = 0
 
     def update(self):
-        self.animation_time += 1
         self.rect.topleft = (self.pos[0], self.pos[1])
 
         # Check for player entering
@@ -32,22 +30,11 @@ class Portal:
         x = self.rect.x - offset[0]
         y = self.rect.y - offset[1]
         w, h = self.size
-        t = self.animation_time
 
-        # Subtle breathing brightness
-        pulse = 0.1 * math.sin(t * 0.05)
 
         # Outer frame (solid)
         frame_rect = pygame.Rect(x - 2, y - 2, w + 4, h + 4)
         pygame.draw.rect(surf, self.outline_color, frame_rect, border_radius=4)
-
-        # Inner glow (static but slightly shifting brightness)
-        main = (
-            min(255, int(self.main_color[0] * (1 + pulse))),
-            min(255, int(self.main_color[1] * (1 + pulse))),
-            min(255, int(self.main_color[2] * (1 + pulse)))
-        )
-        pygame.draw.rect(surf, main, (x, y, w, h), border_radius=3)
 
         # Slightly darker inner rectangle for depth
         inner_rect = pygame.Rect(x + 3, y + 3, w - 6, h - 6)
